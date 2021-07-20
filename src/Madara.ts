6494625bdb015def946cf97066ceeb911f01f1bc
Narin <narin.tana@hotmail.com>
2021-07-20 16:41:21 +0700

    Add empty callback

--- src/Madara.ts
+++ src/Madara.ts
@@ -19,7 +19,7 @@ import {
 import { Parser } from './MadaraParser'
 import { URLBuilder } from './MadaraHelper'
 
-const BASE_VERSION = '2.0.0'
+const BASE_VERSION = '2.0.1'
 export const getExportVersion = (EXTENSION_VERSION: string): string => {
     return BASE_VERSION.split('.').map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index])).join('.')
 }
@@ -203,6 +203,7 @@ export abstract class Madara extends Source {
                 }))
             }
         }
+        mangaUpdatesFoundCallback(createMangaUpdates({ids: []}))
     }
 
     /**

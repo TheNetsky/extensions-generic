ce9813e96040ae32699a14d6057ff074b352539e
Marek Veselý <57869222+notmarek@users.noreply.github.com>
2021-11-09 20:06:10 +0100

    Add WDScans to Madara (#13)
    * Add WDScans to Madara
    
    * Set contentRating to everyone

---
+++ src/WDScans/WDScans.ts
@@ -0,0 +1,38 @@
+import {
+    ContentRating,
+    LanguageCode,
+    SourceInfo,
+    TagType
+} from 'paperback-extensions-common'
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const WDSCANS_DOMAIN = 'https://wdscans.com'
+
+export const WDScansInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'WickedDragon Scans',
+    description: 'Extension that pulls manga from wdscans.com',
+    author: 'NotMarek',
+    authorWebsite: 'http://github.com/notmarek',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: WDSCANS_DOMAIN,
+    sourceTags: [
+        {
+            text: 'Notifications',
+            type: TagType.GREEN
+        }
+    ]
+}
+
+export class WDScans extends Madara {
+    baseUrl: string = WDSCANS_DOMAIN
+    languageCode: LanguageCode = LanguageCode.ENGLISH
+    override hasAdvancedSearchPage = true
+    override sourceTraversalPathName = 'manga'
+    override userAgentRandomizer = ''
+    override alternativeChapterAjaxEndpoint = true
+}

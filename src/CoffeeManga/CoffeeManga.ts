ecf0634bb103b87cb562f331ae68b74937eec6c4
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2022-08-15 11:24:31 +0200

    Add CoffeeManga

---
+++ src/CoffeeManga/CoffeeManga.ts
@@ -0,0 +1,41 @@
+import {
+    ContentRating,
+    LanguageCode,
+    SourceInfo,
+    TagType
+} from 'paperback-extensions-common'
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+
+const DOMAIN = 'https://coffeemanga.com'
+
+export const CoffeeMangaInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'CoffeeManga',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [
+        {
+            text: 'Notifications',
+            type: TagType.GREEN
+        }
+    ]
+}
+
+export class CoffeeManga extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    languageCode: LanguageCode = LanguageCode.ENGLISH
+
+    override alternativeChapterAjaxEndpoint = true
+    
+    override hasAdvancedSearchPage = true
+}

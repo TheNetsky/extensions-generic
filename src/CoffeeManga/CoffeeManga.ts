6b0245c38822df99183471d3001db498912e011c
Netsky <56271887+TheNetsky@users.noreply.github.com>
2022-12-22 07:15:44 +0100

    CoffeeManga | Update Domain

--- src/CoffeeManga/CoffeeManga.ts
+++ src/CoffeeManga/CoffeeManga.ts
@@ -10,10 +10,10 @@ import {
 } from '../Madara'
 
 
-const DOMAIN = 'https://coffeemanga.com'
+const DOMAIN = 'https://coffeemanga.love'
 
 export const CoffeeMangaInfo: SourceInfo = {
-    version: getExportVersion('0.0.0'),
+    version: getExportVersion('0.0.1'),
     name: 'CoffeeManga',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',

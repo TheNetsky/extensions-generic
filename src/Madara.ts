abe6daaf6923e25ea0099920daa5ab91db30b4be
Ivan Matthew <76107136+Ivanmatthew@users.noreply.github.com>
2023-04-16 03:00:03 +0200

    Update base version

--- src/Madara.ts
+++ src/Madara.ts
@@ -25,7 +25,7 @@ import {
     sourceSettings
 } from './MadaraSettings'
 
-const BASE_VERSION = '2.2.5'
+const BASE_VERSION = '2.2.6'
 export const getExportVersion = (EXTENSION_VERSION: string): string => {
     return BASE_VERSION.split('.').map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index])).join('.')
 }

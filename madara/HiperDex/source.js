69638ea07448b57652664b2c3451a00d8206e04e
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-05-25 21:27:29 +0000

    Deploying to gh-pages from @ TheNetsky/extensions-generic@86baa7c3f328ed2b53a9cfb810e39fb40d00199d 🚀

--- madara/HiperDex/source.js
+++ madara/HiperDex/source.js
@@ -19372,7 +19372,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.HiperDex = exports.HiperDexInfo = void 0;
 const paperback_extensions_common_1 = require("paperback-extensions-common");
 const Madara_1 = require("../Madara");
-const DOMAIN = 'https://hiperdex.net';
+const DOMAIN = 'https://hiperdex.com';
 exports.HiperDexInfo = {
     version: Madara_1.getExportVersion('0.0.4'),
     name: 'HiperDex',
